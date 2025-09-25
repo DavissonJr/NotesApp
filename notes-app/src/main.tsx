8708905d9222ee69a70352cff0fc71c0e35b2658
Dávisson Falcão <davissonbeat@hotmail.com>
2025-09-24 23:07:10 -0300

    build: limpar a estrutura base do react

--- notes-app/src/main.tsx
+++ notes-app/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<React.StrictMode>
+<App />
+</React.StrictMode>
 )
